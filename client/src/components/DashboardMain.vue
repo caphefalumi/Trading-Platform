970f6a72ebea4fc12eeb0e211339a1a372c4ac3c
giahieu090806 <giahieutruongngoc@gmail.com>
2025-10-16 15:20:31 +0700

    Upaded code in file DashboardMain.vue in folder /client/src/components

--- client/src/components/DashboardMain.vue
+++ client/src/components/DashboardMain.vue
@@ -244,6 +244,10 @@ onMounted(async () => {
   await fetchMarketInsights(); // 🟢 Fetch real data here
   await nextTick();
   await loadChart(selectedChartTab.value);
+
+  setInterval(async () => { // 🟢 Refresh market insights every 90s
+    await fetchMarketInsights()
+  }, 1500)
 });
 </script>
 <style scoped>
